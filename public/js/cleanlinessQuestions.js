8cde9df3e66f8e0a4ff041e09c9980da3b473e3e
Jae <jjyhong123@gmail.com>
2018-09-08 14:00:00 -0700

    added finance and cleanliness questions

---
+++ public/js/cleanlinessQuestions.js
@@ -0,0 +1,97 @@
+const cleanlinessQuestions =
+    [
+        {
+            "question": "When you cook do you:",
+            "options": [
+                "clean as you go",
+                "clean after you're done cooking",
+                "clean after you're done eating",
+                "clean when you feel like it",
+                "hope someone else cleans it"
+            ]
+        },
+        {
+            "question": "where do clothes go after you take them off?",
+            "options": [
+                "the floor of whatever room you take them off in",
+                "the floor of your room",
+                "the hamper",
+                "back on the shelf/hanger",
+                "directly into the laundry until you have a full load"
+            ]
+        },
+        {
+            "question": "how often do you clean the counters:",
+            "options": [
+                "whenever they need it",
+                "daily",
+                "weekly",
+                "monthly",
+                "clean the counters?"
+            ]
+        },
+        {
+            "question": "how often should the floors be cleaned:",
+            "options": [
+                "daily",
+                "weekly",
+                "monthly",
+                "yearly",
+                "Why would I clean the floors?"
+            ]
+        },
+        {
+            "question": "you spill a drink on the carpet do you:",
+            "options": [
+                "deal with it later",
+                "clean it up",
+                "yell for your roommate",
+                "cover it so you don't have to look at it"
+            ]
+        },
+        {
+            "question": "You had a party last night and the house is a mess, what do you do?",
+            "options": [
+                "split the duties with your roommate",
+                "clean up everything",
+                "you'll deal with it later",
+                "the house looks fine"
+            ]
+        },
+        {
+            "question": "How often do you clean your bathroom:",
+            "options": [
+                "weekly",
+                "as needed",
+                "daily",
+                "monthly"
+            ]
+        },
+        {
+            "question": "How often should the refrigerator be cleaned out:",
+            "options": [
+                "daily",
+                "weekly",
+                "as needed",
+                "only throw out what's expired"
+            ]
+        },
+        {
+            "question": "__ is the step that removes harmful microorganisms on a food contact surface.",
+            "options": [
+                "scrubbing",
+                "scratching",
+                "sanitizing",
+                "cleaning"
+            ]
+        },
+        {
+            "question": "To prevent _______________of food, cleaning supplies, equipment and chemicals should be stored separately and well away from food, dishes, utensils and food contact surfaces.",
+            "options": [
+                "Freezing",
+                "Contamination",
+                "Rotting",
+                "Burning"
+            ]
+        }
+    ];
---
+++ public/js/financeQuestions.js
@@ -0,0 +1,47 @@
+const financeQuestions =
+    [{
+        "question": "Do you have a monthly budget?",
+        "options": ["Yes", "No"]
+    },
+    {
+        "question": "When you see something you want but it doesnt fit in your budget do you:",
+        "options": ["buy it anyway", "put it on a credit card", "look for sales on said item", "cut back in other areas so you can afford it", "save a portion of the cost over a few months"]
+    },
+    {
+        "question": "Divide your monthly income by your monthly debt payments, is the result:",
+        "options": [">1", "equal to 1", "<1 but >0.5", "<0.5"]
+    },
+    {
+        "question": "How many credit cards do you have?",
+        "options": ["1", "2", "3", "more than 3"]
+    },
+    {
+        "question": "If you were to lose your entire income tomorrow, what would you do?",
+        "options": ["apply for a loan", "use a credit card", "ask your parents", "use your savings"]
+    },
+    {
+        "question": "What would you do when you don't have enough money for rent?",
+        "options": ["Ask for an extension", "Get a loan", "Cut back on expenses to supplement rent", "Use my emergency savings"]
+    },
+    {
+        "question": "A store is offering 10% off for customers who use their store card. This is an example of making purchases using",
+        "options": ["debit", "currency", "credit", "cash"]
+    },
+    {
+        "question": "What is the term for the money you earn?",
+        "options": ["income", "savings", "interest", "expense"]
+    },
+    {
+        "question": "Agreement to receive cash, goods, or services now and pay for them at a later date",
+        "options": ["savings", "income", "credit", "investment"]
+    },
+    {
+        "question": "A card that allows money to be withdrawn from your account",
+        "options": ["credit card", "debit card", "check", "gift certificate"]
+    },
+    {
+        "question": "In order to effectively manage money, you need a",
+        "options": ["budget", "computer", "online checking account", "high paying job"]
+    }];
+
+module.exports = financeQuestions;
