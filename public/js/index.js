57f2bb67efe9e2db5b76cc31a968d98b9f694e38
Diana <dianajulaton@gmail.com>
2018-09-11 22:37:31 -0700

    added more animate stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 // Get references to page elements
-var registerBtn = $("#register")
-var loginBtn = $("#loginBtn")
+var registerBtn = $("#register");
+var loginBtn = $("#loginBtn");
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveUser: function (User) {
+  saveUser: function(User) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -19,45 +19,77 @@ var API = {
       url: "api/login",
       type: "GET"
     });
-  },
+  }
 };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleLogin = function (event) {
-var login = $("#emailLogin").val().trim()
-var password = $("#passwordLogin").val().trim()
+var handleLogin = function(event) {
+  var login = $("#emailLogin")
+    .val()
+    .trim();
+  var password = $("#passwordLogin")
+    .val()
+    .trim();
 
-  API.login(login, password)
+  API.login(login, password);
+};
 
-}
-
-var handleFormSubmit = function (event) {
-
+var handleFormSubmit = function(event) {
   var User = {
-    user: $("#name").val().trim(),
-    password: $("#password").val().trim(),
-    email: $("#email").val().trim(),
-    phone: $("#phoneNumber").val().trim(),
-    bio: $("#bio").val().trim(),
-    hobbies: $("#hobbies").val().trim(),
-    age: parseInt($("#age").val().trim()),
-    gender: $("#gender").val().trim(),
-    budget: parseFloat($("#budget").val().trim()),
+    user: $("#name")
+      .val()
+      .trim(),
+    password: $("#password")
+      .val()
+      .trim(),
+    email: $("#email")
+      .val()
+      .trim(),
+    phone: $("#phoneNumber")
+      .val()
+      .trim(),
+    bio: $("#bio")
+      .val()
+      .trim(),
+    hobbies: $("#hobbies")
+      .val()
+      .trim(),
+    age: parseInt(
+      $("#age")
+        .val()
+        .trim()
+    ),
+    gender: $("#gender")
+      .val()
+      .trim(),
+    budget: parseFloat(
+      $("#budget")
+        .val()
+        .trim()
+    ),
     finance_score: 0,
     personality_score: 0,
     clean_score: 0,
-    job_title: $("#jobTitle").val().trim(),
+    job_title: $("#jobTitle")
+      .val()
+      .trim(),
     employed: 0,
-    city: $("#city").val().trim(),
-    zip: parseInt($("#zip").val().trim()),
+    city: $("#city")
+      .val()
+      .trim(),
+    zip: parseInt(
+      $("#zip")
+        .val()
+        .trim()
+    )
   };
 
   console.log(User);
-  API.saveUser(User)
+  API.saveUser(User);
   //create new api route for new user, create ajax to call body of user
 };
 
 // Add event listeners to the submit and delete buttons
 registerBtn.on("click", handleFormSubmit);
-loginBtn.on("click", handleLogin)
+loginBtn.on("click", handleLogin);
