590c0845ca8233dcc1ccbca40a62e73f2f791910
Jae <jjyhong123@gmail.com>
2018-09-13 23:33:24 -0700

    alpha

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,50 @@
-// Get references to page elements
-
-var registerBtn = $("#register");
-
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveUser: function (User) {
-    return $.ajax({
+    $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
       url: "api/newuser",
       data: JSON.stringify(User)
+    }).then(function (data) {
+      location.href = "/finance";
     });
   },
-  /*
-  login: function() {
-    return $.ajax({
-      url: "api/login",
-      type: "GET"
+
+  login: function(creds) {
+    $.ajax({
+      url: "/api/login",
+      type: "POST",
+      data: creds
+    }).then(function(data){
+      console.log(data.value);
+      if(data.value===true){
+      location.href="/dashboard"
+      }else{
+        console.log('wrong password')
+      }
+      
     });
   },
-  */
+  
 };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
 var handleLogin = function (event) {
-  event.preventDefault();
-  var login = $("#emailLogin").val().trim()
-  var password = $("#passwordLogin").val().trim()
+  
+  var creds = {
+    login: $("#emailLogin").val().trim(),
+    password: $("#passwordLogin").val().trim()
+  };
+  sessionStorage.setItem("userSession", creds.login);
+  API.login(creds);
 
-  API.login(login, password)
 }
 
-var handleFormSubmit = function (event) {
+var handleFormSubmit = function () {
   var User = {
     user: $("#name").val().trim(),
     password: $("#password").val().trim(),
@@ -53,11 +63,118 @@ var handleFormSubmit = function (event) {
     city: $("#city").val().trim(),
     zip: parseInt($("#zip").val().trim()),
   };
-
-  console.log(User);
+  sessionStorage.setItem("email", User.email);
+  sessionStorage.setItem("zip", User.zip);
   API.saveUser(User)
-  //create new api route for new user, create ajax to call body of user
 };
 
-// Add event listeners to the submit and delete buttons
+// REGISTRATION PAGE STUFF
+// -----------------------------------------
+// Add event listener to the register button
+// $('#modal-incorrectLogin').modal('show');
+var loginBtn = $("#loginBtn");
+loginBtn.on("click", handleLogin);
+
+var registerBtn = $("#register");
 registerBtn.on("click", handleFormSubmit);
+// END OF REGISTRATION PAGE STUFF
+// -----------------------------------------
+
+// FINANCE STUFF
+// ---------------------------------------------------
+$(document).ready(function () {
+  $("#submitFinance").on("click", function (event) {
+    var scores = [];
+
+    // eventually change j < 10
+    for (let j = 0; j < 10; j++) {
+      var name = "financeQuestion" + j;
+      var radios = document.getElementsByName(name);
+
+      for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+          // do whatever you want with the checked radio
+          scores.push(parseInt(radios[i].value) + 1);
+
+          // only one radio can be logically checked, don't check the rest
+          break;
+        }
+      }
+    }
+
+    var sumOfScores = 0;
+    for (let i = 0; i < scores.length; i++) {
+      sumOfScores += scores[i];
+    }
+    var avgOfScores = sumOfScores / scores.length;
+    alert("Your finance score is: " + avgOfScores);
+
+    function updatePost(avgScore) {
+      $.ajax({
+        method: "PUT",
+        url: "/api/finance",
+        data: {
+          financeScore: avgScore,
+          email: sessionStorage.getItem("email")
+        }
+      })
+        .then(function () {
+          location.href = "/cleanliness";
+        });
+    };
+    updatePost(avgOfScores);
+  });
+});
+// END OF FINANCE STUFF
+// -----------------------------------------------------------
+
+
+// -----------------------------------------------------------
+// CLEANLINESS STUFF
+$(document).ready(function () {
+  $("#submitCleanliness").on("click", function (event) {
+    var scores = [];
+
+    // eventually change j < 10
+    for (let j = 0; j < 10; j++) {
+      var name = "cleanlinessQuestion" + j;
+      var radios = document.getElementsByName(name);
+
+      for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+          // do whatever you want with the checked radio
+          scores.push(parseInt(radios[i].value) + 1);
+
+          // only one radio can be logically checked, don't check the rest
+          break;
+        }
+      }
+    }
+
+    var sumOfScores = 0;
+    for (let i = 0; i < scores.length; i++) {
+      sumOfScores += scores[i];
+    }
+    var avgOfScores = sumOfScores / scores.length;
+    alert("Your cleanliness score is: " + avgOfScores);
+
+    function updatePost(avgScore) {
+      $.ajax({
+        method: "PUT",
+        url: "/api/cleanliness",
+        data: {
+          cleanScore: avgScore,
+          email: sessionStorage.getItem("email")
+        }
+      })
+        .then(function () {
+          location.href = "/personality";
+        });
+    };
+
+    updatePost(avgOfScores);
+  });
+});
+// END OF CLEANLINESS STUFF
+// -------------------------------------------
+
